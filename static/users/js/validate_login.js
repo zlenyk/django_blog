288eb2f2de9565f114cd0e42b5dd74a825fb3ee1
zlenyk <zygmuntlenyk@gmail.com>
2016-04-10 16:34:45 +0200

    new look

--- static/users/js/validate_login.js
+++
@@ -1,8 +0,0 @@
-function validateForm(){
-    x = document.forms["loginForm"]["username"].value;
-    if( x == null || x == "" ){
-        console.log("asd");
-        alert("Login must be filled");
-        return false;
-    }
-}
