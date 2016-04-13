d35280e8e0a4153823b23535dc02a71d85a04823
zlenyk <zygmuntlenyk@gmail.com>
2016-04-14 00:55:58 +0200

    not logged users can't watch classified blog via url

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.shortcuts import get_object_or_404
-
+from django.contrib.auth.decorators import login_required
+import users
 def index(request):
     all_posts = Post.objects.all().filter(classified=False).order_by('-publish_date')
     context = {'post_list': all_posts}
     return render(request,'blog/index.html',context)
 
+@login_required
 def classified(request):
     all_posts = Post.objects.all().filter(classified=True).order_by('-publish_date')
     context = {'post_list': all_posts}
@@ -14,6 +16,14 @@ def classified(request):
 
 def post(request,slug,id):
     post = get_object_or_404(Post,pk=id)
-    all_posts = Post.objects.all().filter(classified=False).order_by('-publish_date')
+    all_posts = []
+    if post.classified == True:
+        if request.user.is_authenticated():
+            print(request.user)
+            all_posts = Post.objects.all().filter(classified=True).order_by('-publish_date')
+        else:
+            return users.views.login(request)
+    else:
+        all_posts = Post.objects.all().filter(classified=False).order_by('-publish_date')
     context = {'post': post,'post_list': all_posts}
     return render(request,'blog/post.html',context)
--- person/settings/base.py
+++ person/settings/base.py
@@ -126,3 +126,5 @@ REDACTOR_UPLOAD = 'static/img/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+
+LOGIN_URL = '/users/login/'
